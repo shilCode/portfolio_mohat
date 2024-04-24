7bc202d56cc4e107f252cc7d15dd1cf463d57854
shilcode <2850542@gmail.com>
2024-04-24 13:37:21 +0200

    new cv updated

--- portfolio-first/src/components/Home.jsx
+++ portfolio-first/src/components/Home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-red-400 '>Hi 👋, I am</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-gray-200'>Mohat Shil</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold  text-gray-400'>Automation QA JS/TS 📱🕸️🖥️</h2>
+        <h2 className='text-4xl sm:text-7xl font-bold  text-gray-400'>Automation QA 📱🕸️🖥️</h2>
         <p className='text-gray-500 mx-w-[700px]'>with 5+ years of Experience</p>
         <div>
         <button className='text-white group border-2 px-6 py-3 my-2 items-center flex hover:bg-pink-600 hover:border-pink-600' >
-             <a href={cv} download='mohat-shil.pdf'>Download Professional CV</a>
+             <a href={cv} download='mohat-shil.pdf'>Download CV</a>
              <span className='group-hover:rotate-90 duration-300'></span>
              </button>
         </div>
