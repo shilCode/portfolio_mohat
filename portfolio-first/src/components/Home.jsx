6ac1c0dadf7ff0bdacf76967ab3a70c20d892153
shilcode <2850542@gmail.com>
2024-07-22 21:09:16 +0200

    latest

--- portfolio-first/src/components/Home.jsx
+++ portfolio-first/src/components/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div name='home' className='w-auto sm:w-auto lg:w-auto lg:h-auto md:h-auto sm:h-auto bg-[#292524]  overflow-hidden'>
     
-      {/* Header */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col h-full font-mono text-white'>
+      {/* Header */}
         <div>
         <div className='  flex  justify-between items-center pt-10 pb-10  gap-10 '>
             <div >
@@ -33,17 +33,17 @@ const Home = () => {
             <ul> 🎯 My goal is to become a Cloud Architect and contribute to open-source projects.</ul>
           </div>
         </div>
-      </div>
+        </div>
 
-      <hr />
         {/* Experience */}
+      <hr />
         <div >
         <h2 className='  font-mono pt-5 pb-4 text-2xl'>Work Experience </h2>
         <div>
 
         <div className=' flex pb-10 '>
            <div className=' w-1/2'>
-            <h3>ABB Business Services</h3>
+            <a href='https://global.abb/group/en' target="_blank">ABB Business Services</a>
             <div className='ms-10 '>
             <ul >
                 QA Automation Engineer
@@ -58,7 +58,7 @@ const Home = () => {
            </div>
 
            <div className=' w-1/2'>
-            <h3>EPAM</h3>
+            <a href='https://www.epam.com/' target="_blank">EPAM Systems</a>
             <div className='ms-10 '>
             <ul >
                 Senior Software Engineer in Test
@@ -75,7 +75,7 @@ const Home = () => {
           </div>
           <div className='pb-10 flex '>
            <div className=' w-1/2'>
-            <h3>Keywords Studios</h3>
+            <a href='https://www.keywordsstudios.com/en/' target='_blank'>Keywords Studios</a>
             <div className='ms-10 '>
             <ul >
                 QA Test Lead
@@ -90,7 +90,7 @@ const Home = () => {
            </div>
 
            <div className='w-1/2'>
-            <h3>Testronic</h3>
+            <a href='https://www.testroniclabs.com/' target='_blank' >Testronic</a>
             <div className='ms-10 '>
             <ul >
                 QA Associate Test Lead - Games
@@ -107,9 +107,8 @@ const Home = () => {
           </div>
 
         </div>
-        </div>
       <hr />
-
+        </div>
       <hr />
         {/* Tech Stack */}
         <div className='pb-10' >
@@ -117,38 +116,38 @@ const Home = () => {
               <div className=' flex'>
                   <div className=' pb-10 w-1/2'>
                     <div>E2E Testing:
-                    <div className='ms-10 underline'>
-                    <a href="https://playwright.dev/">Playwright</a><br/>
-                    <a href="https://www.cypress.io/">Cypress</a><br/>
-                    <a href="https://pptr.dev/">Puppeteer</a><br/>
-                    <a href="https://webdriver.io/">WebdriverIO</a><br/>
-                    <a href="https://www.protractortest.org/#/">Protractor</a><br/>
-                    <a href="https://jestjs.io/">Jest</a><br/>
+                    <div className='ms-10'>
+                    <a href="https://playwright.dev/" target="_blank">Playwright</a><br/>
+                    <a href="https://www.cypress.io/" target="_blank">Cypress</a><br/>
+                    <a href="https://pptr.dev/" target="_blank">Puppeteer</a><br/>
+                    <a href="https://webdriver.io/" target="_blank">WebdriverIO</a><br/>
+                    <a href="https://www.protractortest.org/#/" target="_blank">Protractor</a><br/>
+                    <a href="https://jestjs.io/" target="_blank">Jest</a><br/>
                     </div>
                     </div>
                   </div>
 
                   <div className='flex w-1/2'>
-                    <div >Tools <br/>
-                    <a className='ms-10 underline' href="https://www.postman.com/">Postman</a><br/>
-                    <a className='ms-10 underline' href="https://jmeter.apache.org/">Jmeter</a><br/>
-                    <a className='ms-10 underline' href="https://www.docker.com/">Docker</a><br/>
-                    <a className='ms-10 underline' href="https://kubernetes.io/">Kubernetes</a><br/>
-                    <a className='ms-10 underline' href="https://www.jenkins.io/">Jenkins</a><br/>
-                    <a className='ms-10 underline' href="https://azure.microsoft.com/en-us/products/devops">ADO Pipelines</a>
+                    <div >Tools: <br/>
+                    <a className='ms-10 ' href="https://www.postman.com/" target="_blank">Postman</a><br/>
+                    <a className='ms-10 ' href="https://jmeter.apache.org/" target="_blank">Jmeter</a><br/>
+                    <a className='ms-10 ' href="https://www.docker.com/" target="_blank">Docker</a><br/>
+                    <a className='ms-10 ' href="https://kubernetes.io/" target="_blank">Kubernetes</a><br/>
+                    <a className='ms-10 ' href="https://www.jenkins.io/" target="_blank">Jenkins</a><br/>
+                    <a className='ms-10 ' href="https://azure.microsoft.com/en-us/products/devops" target="_blank">ADO Pipelines</a>
                   
                   </div> 
               </div>
                   <div className=' w-1/2'>Programming Languages 
-                    <div className='ms-10 underline '>
-                    <a href="https://javascript.info/">JavaScript</a><br/>
-                    <a href="https://typescriptlang.org/">TypeScript</a><br/>
+                    <div className='ms-10  '>
+                    <a href="https://javascript.info/" target="_blank">JavaScript</a><br/>
+                    <a href="https://typescriptlang.org/" target="_blank">TypeScript</a><br/>
                   </div> <br/>
               </div>
-    </div>
+      </div>
 
-        </div>
         <hr />
+        </div>
             <footer className=' bg-none font-mono pt-5'>
              //
             </footer>
@@ -158,5 +157,4 @@ const Home = () => {
     </div>
   )
 }
-
-export default Home
+export default Home;
