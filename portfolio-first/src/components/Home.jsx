fe57432a69752c9d87d4b8e9f1ab22ca9f3c48e4
Mohat Shil <120472338+shilCode@users.noreply.github.com>
2024-07-28 00:35:30 +0200

    Update Home.jsx

--- portfolio-first/src/components/Home.jsx
+++ portfolio-first/src/components/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
         <h2 className='  font-mono pt-5 lg:text-2xl md:text-2xl sm:text-xl font-bold'>Software Developer in Test 📱🕸️🖥️</h2>
           <div className=' font-mono pt-5 pb-10 '>
             <ul className='pb-2'> 👋🏼 I’m Mohat Shil, a SDET with 5+ years of experience in multiple MNCs. </ul>
-            <ul className='pb-2'> 🎓 Graduated in 2021 with a Computer Engineering bechelors degree.</ul>
+            <ul className='pb-2'> 🎓 Graduated in 2021 with a Computer Engineering Bechalors degree.</ul>
             <ul className='pb-2'>  🗡  As a passionate Test Automation Enthusiast, I specialize in cutting-edge technologies and testing frameworks.</ul>
             <ul className='pb-2'> 🔍 Explore my latest projects and contributions on my <a href='https://github.com/shilCode' className=' underline' target="_blank">GitHub</a>.</ul>
             <ul className='pb-2'> 📚 I'm currently learning about DevOps and Cloud Computing.</ul>
