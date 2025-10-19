c410dd7a5d38e9b17a5ea5d2d447b714befcf4bf
shilcode <2850542@gmail.com>
2025-10-19 21:51:53 +0200

    npm audit fix

--- src/components/common/ExternalLink.jsx
+++ src/components/common/ExternalLink.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ExternalLink = ({ href, children, className = "" }) => {
   return (
     <a
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 import { FaGithub, FaGraduationCap } from "react-icons/fa";
 import { BsCodeSquare } from "react-icons/bs";
